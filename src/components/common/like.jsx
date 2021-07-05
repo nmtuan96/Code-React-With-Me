59899a695aa15262a1b3429618db4b9b3d34afd1
nmtuan96 <nmtuanhumg96@gmail.com>
2021-07-05 14:29:34 +0700

    pagination

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,11 +1,9 @@
 import React , { Component} from 'react';
-import FontAwesome from 'react-fontawesome';
 
-class Like extends Component{
-    state ={}
-    render(){
-        return <FontAwesome  icon="heart"/>;
-        
-    }
+const Like = props => {
+    let studentsLike = "fa fa-heart";
+    if (!props.liked) studentsLike += "-o"
+    return <i onClick={props.onClick} style={{cursor: 'pointer'}} className={studentsLike} aria-hidden="true"></i>;
 }
+
 export default Like;
