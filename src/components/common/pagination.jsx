80888248352907349c60a748d10d3199019c6df1
nmtuan96 <nmtuanhumg96@gmail.com>
2021-07-05 14:51:42 +0700

    pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 
 const Pagination = props => {
     const { itemsCount, pageSize, currentPage , onPageChange } = props;
@@ -18,6 +19,12 @@ const Pagination = props => {
             </ui>
         </nav>
      );
+}
+Pagination.propTypes={
+    itemsCount: PropTypes.number.isRequired, 
+    pageSize: PropTypes.number.isRequired, 
+    currentPage: PropTypes.number.isRequired, 
+    onPageChange: PropTypes.func.isRequired
 }
  
 export default Pagination
