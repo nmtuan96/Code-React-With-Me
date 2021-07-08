4e1f02b170553a1a0e81f4e7b47f4ff3614fb82e
nmtuan96 <nmtuanhumg96@gmail.com>
2021-07-08 16:33:08 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import NavBar from './components/navBar';
+import { Route, Switch, Redirect } from 'react-router';
 import Students from './components/student';
+import Groups from './components/groups';
+import NotFound from './components/notFound';
+import Teachers from './components/teachers';
+import DetailStudent from './components/detailStudent';
 
 function App() {
   return (
     <main className="container">
-      <Students />
+      <NavBar />
+      <Switch>
+        <Route path="/students/:id" component={DetailStudent} />
+        <Route path="/students" component={Students} />
+        <Route path="/classes" component={Groups} />
+        <Route path="/not-found" component={NotFound} />
+        <Route path="/teachers" component={Teachers} />
+        <Redirect from="/" exact to="/students" />
+        <Redirect from="/not-found" />
+      </Switch>
     </main>
   );
 }
---
+++ src/components/detailStudent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DetailStudent = () => {
+    return ( 
+        <h1>Detail Student</h1>
+     );
+}
+ 
+export default DetailStudent;
